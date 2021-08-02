3bb5de3240c70dae9791a90d6e85c91cea485c13
stuartj073 <stuartj073@gmail.com>
2021-08-02 16:14:54 +0000

    core java functions written down

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,18 @@
+function toggleRules(){
+console.log("hello");
+}
+
+alert("what up");
+
 if(document.readyState === "loading"){
     document.addEventListener("DOMContentLoaded", startGame())
 }   else {
     startGame()
+}
+
+function startGame() {
+    let timer = document.getElementById("score");
+    timer = --;
 }
 
 
