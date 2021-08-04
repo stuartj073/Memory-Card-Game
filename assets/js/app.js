0a8a65dee7224028cf785b37050ed7724641c780
stuartj073 <stuartj073@gmail.com>
2021-08-04 21:47:03 +0000

    updating of functions for game

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,30 +1,56 @@
+if(document.readyState === "loading") {
+    document.addEventListener("DOMContentLoaded", function(){
+
+    })
+}
+
+function shuffleCards(cards){
+    // using the Fisher Yates shuffle theory
+  
+      
+      var currentIndex = array.length,  randomIndex;
+    
+      // While there remain elements to shuffle...
+      while (0 !== currentIndex) {
+    
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+    
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [
+          array[randomIndex], array[currentIndex]];
+      }
+    
+      return cards.Array;
+  }
+
 function toggleRules(){
 console.log("hello");
 }
 
-//$("#game-rules").click(function(){
-//    ("button").toggle("slow");
-// })
-
-let 
-
-if(document.readyState === "loading"){
-    document.addEventListener("DOMContentLoaded", function(){
-
-    })
-
-
+$("#game-rules").click(function(){
+    ("button").slideToggle("slow");
+ })
 
 function startGame()
 
-function flipCard() {
-    $("card").click(function(){
-        $(this).removeClass("card-front").addClass("card-back");
-    })
+function flipCard(){
+    let cards = document.getElementsByClassName('card');
+    
+    for(let i = 0; i< cards.length; i++){
+        cards[i].addEventListener('click', function(){
+            cards.toggle.classList.remove('card-front').clasList.add('card-back')
+        })
+    }
 }
 
+let cards = document.getElementsByClassName('card')
+
 function checkCards() {
-    let card 1 = docu
+    for(card of cards) {
+        card.addEventListener('click', flipCard())
+    }
 }
 
 function correctMatch() {
@@ -38,6 +64,12 @@ function correctMatch() {
 function unflipCards() {
 
 }
+
+function matchedCards(){
+    
+}
+
+function unmatchedCards
 
 function increaseScore(){
     let score = document.getElementById('score').innerHTML;
