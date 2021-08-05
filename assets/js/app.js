a6807c4682368efb8a9834d10a93cdcf859490a0
stuartj073 <stuartj073@gmail.com>
2021-08-05 17:50:57 +0000

    updated some information in functions

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,33 @@
 if(document.readyState === "loading") {
     document.addEventListener("DOMContentLoaded", function(){
-
+        console.log("Hi")
     })
 }
+
+function consoleTest(){
+    let score = document.getElementById("score");
+
+    score.addEventListener('click', function(){
+        console.log("bye");
+    })
+}
+
+const cards = document.querySelector(".card");
+
+function hi(){
+    let scores = document.getElementById('score');
+
+    score.addEventListener('click', function(){
+        alert("uh")
+    })
+}
+
 
 function shuffleCards(cards){
     // using the Fisher Yates shuffle theory
   
       
-      var currentIndex = array.length,  randomIndex;
+      var currentIndex = cards.Array.length,  randomIndex;
     
       // While there remain elements to shuffle...
       while (0 !== currentIndex) {
@@ -18,8 +37,8 @@ function shuffleCards(cards){
         currentIndex--;
     
         // And swap it with the current element.
-        [array[currentIndex], array[randomIndex]] = [
-          array[randomIndex], array[currentIndex]];
+        [cards.Array[currentIndex], cards.Array[randomIndex]] = [
+          cards.Array[randomIndex], cards.Array[currentIndex]];
       }
     
       return cards.Array;
@@ -37,7 +56,6 @@ function startGame()
 
 function flipCard(){
     let cards = document.getElementsByClassName('card');
-    
     for(let i = 0; i< cards.length; i++){
         cards[i].addEventListener('click', function(){
             cards.toggle.classList.remove('card-front').clasList.add('card-back')
@@ -53,23 +71,20 @@ function checkCards() {
     }
 }
 
-function correctMatch() {
-    if(card1 === card 2) {
-        increaseScore()
-    }   else {
-        unflipCards()
-    }
-}
-
 function unflipCards() {
 
 }
 
-function matchedCards(){
-    
+function matchedCards(cards1, cards2){
+    var matchedCards = [];
+
+    if(cards[0]===cards[1]){
+        matchedCards.push(card1, card2)
+    }
+
 }
 
-function unmatchedCards
+function unmatchedCards()
 
 function increaseScore(){
     let score = document.getElementById('score').innerHTML;
@@ -84,11 +99,29 @@ function increaseScore(){
 
 function increaseFlip()
 
-function timer()
+function timer(){
+    setInterval(function(){
+        let timer = document.getElementById('time').innerText;
+        this.timer--;
+        timer = this.timer.innerText;
+
+        if(timer===0){
+            gameOver();
+        }
+    },1000)
+}
 
 function victory()
 
-function gameOver()
+function gameOver(){
+    
+}
+
+function menuToggle(){
+    $("button").addEventListener('click',function(){
+        $(".game-rules").toggleClass();
+    })
+}
 
 
 
