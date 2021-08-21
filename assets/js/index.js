60827ee39fa1c3270053ebb82f3a2d7092495784
stuartj073 <stuartj073@gmail.com>
2021-08-21 23:04:28 +0000

    JSHint errors fixed for index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,5 @@
+/*jshint esversion: 6 */
+
 // Get the modal
 var modal = document.getElementById("myModal");
 
@@ -10,17 +12,17 @@ var span = document.getElementsByClassName("close")[0];
 // When the user clicks on the button, open the modal
 rules.onclick = function() {
   modal.style.display = "block";
-  console.log("ye")
-}
+  console.log("ye");
+};
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
-}
+};
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-}
+};
